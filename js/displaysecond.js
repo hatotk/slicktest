724cd55f13f272b8ed107660a2dec3aaa040c8ea
toshi <toshi@CL-01>
2024-05-28 01:13:16 +0900

    微調整を行いました。
    ・切り替え時間を4秒、動画表示時間を5秒、静止画表示時間を3秒にしました。
    ・動画が始まる際に頭から再生するようにしました。

--- js/displaysecond.js
+++ js/displaysecond.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   let second = 0;
 
@@ -7,6 +7,11 @@ $(document).ready(function(){
     second = second + 1;
 
     document.getElementById("second").textContent = second;
+
+    var testVideoElement = document.getElementById('testVideo');
+    testVideoElement.addEventListener('timeupdate', function() {
+      document.getElementById("videoCurrentTime").textContent = testVideoElement.currentTime;
+    });
 
   }, 1000);
 
--- js/slicktest.js
+++ js/slicktest.js
@@ -1,23 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $('.your-class').slick({
     arrows: false,
     pauseOnFocus: false,
     pauseOnHover: false,
-    speed: 5000, // 切り替え時間（ミリ秒）
+    speed: 4000, // 切り替え時間（ミリ秒）
     fade: true,
     cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 6000, // 表示時間（ミリ秒）
   });
 
-  $('.your-class').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  $('.your-class').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if (0 == nextSlide) {
+      // スライドを切り替える処理実行する前のタイミングで、
+      // 次のスライドが1番目のスライド（動画のスライド）の時、動画再生位置を設定する。
+      document.getElementById("testVideo").currentTime = 0;
+    }
+  });
+
+  $('.your-class').on('afterChange', function(event, slick, currentSlide) {
     if (0 == currentSlide) {
-      // 1番目のスライドの時、表示時間を6秒に設定する。
-      $('.your-class').slick('slickSetOption', 'autoplaySpeed', 6000, false);
+      // スライドを切り替える処理実行した後のタイミングで、
+      // 現在のスライドが1番目のスライド（動画のスライド）の時、表示時間を5秒に設定する。
+      $('.your-class').slick('slickSetOption', 'autoplaySpeed', 5000, false);
     } else {
-      // 1番目以外のスライドの時、表示時間を4秒に設定する。
-      $('.your-class').slick('slickSetOption', 'autoplaySpeed', 4000, false);
+      // スライドを切り替える処理実行した後のタイミングで、
+      // 現在のスライドが1番目以外のスライド（静止画のスライド）の時、表示時間を3秒に設定する。
+      $('.your-class').slick('slickSetOption', 'autoplaySpeed', 3000, false);
     }
   });
 
